17269a2cb84beaf20d7b9fcc42a628726f4c2344
Soullessly <alexbarthel01@gmail.com>
2022-02-23 14:50:46 -0600

    MVP

--- cards.js
+++ cards.js
@@ -1,38 +1,70 @@
-// Arrays to hold values
-const suits = ["♣","♦", "♥", "♠"]
-const values = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "K"]
+const SUITS = ["♠", "♣", "♥", "♦"]
+const VALUES = [
+  "A",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "10",
+  "J",
+  "Q",
+  "K"
+]
 
-export default class deck {
-    constructor() {
-        this.cards = cards;
-    }
+export default class Deck {
+  constructor(cards = freshDeck()) {
+    this.cards = cards
+  }
 
-    get numberOfCards() {
-        return cards.length;
+  get numberOfCards() {
+    return this.cards.length
+  }
+
+  pop() {
+    return this.cards.shift()
+  }
+
+  push(card) {
+    this.cards.push(card)
+  }
+
+  shuffle() {
+    for (let i = this.numberOfCards - 1; i > 0; i--) {
+      const newIndex = Math.floor(Math.random() * (i + 1))
+      const oldValue = this.cards[newIndex]
+      this.cards[newIndex] = this.cards[i]
+      this.cards[i] = oldValue
     }
-// randomize the deck
-    shuffle() {
-        for (let i  = this.numberOfCards - 1; i > 0; i--) {
-            const  newIndex = math.floor(math.random() * (i + 1))
-            const oldValue = this.card[newIndex]
-            this.cards[newIndex] = this.card[i]
-            this.cards[i] = oldValue
-        }
-    }
+  }
 }
-// Builds the card
-class card {
-    constructor(suit, value) {
-        this.suit = suit;
-        this.value = value;
-    }
+
+class Card {
+  constructor(suit, value) {
+    this.suit = suit
+    this.value = value
+  }
+
+  get color() {
+    return this.suit === "♣" || this.suit === "♠" ? "black" : "red"
+  }
+
+  getHTML() {
+    const cardDiv = document.createElement("div")
+    cardDiv.innerText = this.suit
+    cardDiv.classList.add("card", this.color)
+    cardDiv.dataset.value = `${this.value} ${this.suit}`
+    return cardDiv
+  }
 }
-// Generates the deck
-function generateDeck() {
-    return suits.flatmap(suits => {
-        return values.map(values =>{
-            return new card(suits, values)
-        })
+
+function freshDeck() {
+  return SUITS.flatMap(suit => {
+    return VALUES.map(value => {
+      return new Card(suit, value)
     })
+  })
 }
-
